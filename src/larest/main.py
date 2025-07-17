a0f1e06b2d6b189fa4a417b081d5f8d6f78cba53
Ryan-Reese <thenewteadrinker@gmail.com>
2025-07-17 14:01:51 +0100

    Finished logic of main script

--- src/larest/main.py
+++ src/larest/main.py
@@ -3,6 +3,8 @@ import asyncio
 import logging
 from typing import Any
 
+from tqdm import tqdm
+
 from larest.base import Initiator, Monomer, Polymer
 from larest.parsers import LarestArgumentParser
 from larest.setup import get_config, get_logger
@@ -24,10 +26,54 @@ def main(
         try:
             initiator.run()
         except Exception as err:
-            logger.exception("Failed to run xTB for initiator")
+            logger.exception("Failed to run pipeline for initiator")
             raise SystemExit(1) from err
         else:
-            logger.info("Finished running xTB for initiator")
+            logger.info("Finished running pipeline for initiator")
+
+    # run pipeline for each monomer
+    logger.info("Running pipeline for monomers")
+    for monomer_smiles in tqdm(
+        config["reaction"]["monomers"],
+        desc="Running pipeline for monomers",
+    ):
+        # run xtb for monomer
+        monomer: Monomer = Monomer(
+            smiles=monomer_smiles,
+            args=args,
+            config=config,
+        )
+        try:
+            monomer.run()
+        except Exception:
+            logger.exception(f"Failed to run pipeline for monomer {monomer_smiles}")
+            continue
+        else:
+            logger.info("Finished running pipeline for monomer")
+
+        # run pipeline for each polymer length
+        for polymer_length in tqdm(
+            config["reaction"]["lengths"],
+            desc="Running pipeline for each polymer length",
+        ):
+            # build polymer
+            polymer: Polymer = Polymer(
+                smiles=monomer_smiles,
+                length=polymer_length,
+                args=args,
+                config=config,
+            )
+            try:
+                polymer.run()
+            except Exception:
+                logger.exception(
+                    f"Failed to run pipeline for polymer {monomer_smiles} (length: {polymer_length})",
+                )
+                continue
+            else:
+                logger.info("Finished running pipeline for polymer")
+
+        monomer.compile_monomer_results()
 
 
 if __name__ == "__main__":

06bb83a315872df4cb49287cac6abd7c45edb072
Ryan-Reese <thenewteadrinker@gmail.com>
2025-07-19 15:41:08 +0100

    preparing for small HPC run

--- pipeline.sh
+++ pipeline.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/bash
 #PBS -N LaREST
 #PBS -l walltime=23:59:00
-#PBS -l select=1:ncpus=64:mem=400gb:mpiprocs=64
-
-# NOTE: the above resources fall under a medium CPU PBS job
+#PBS -l select=1:ncpus=16:mem=128gb:mpiprocs=16
 
 # source general config file
 CONFIG_FILE="${PBS_O_WORKDIR}/config/general.conf"
 if [ -e "${CONFIG_FILE}" ]; then
     source "${CONFIG_FILE}"
 fi
+
+# load orca
+module load ORCA/6.1.0-gompi-2023b
 
 # activate conda env
 eval "$("${CONDA_DIR}"/conda shell.bash hook)"
@@ -23,8 +24,6 @@ mkdir -p "${RUN_DIR}"
 # copy config to run directory
 cp -r "${PBS_O_WORKDIR}/${CONFIG_DIR}" "${RUN_DIR}"
 
-# run Step 1 of pipeline (xTB)
-python "${PBS_O_WORKDIR}/${SRC_DIR}/step1.py" -o "${RUN_DIR}" -c "${RUN_DIR}/${CONFIG_DIR}"
+# run LaREST
+python "${PBS_O_WORKDIR}/${SRC_DIR}/main.py" -o "${RUN_DIR}" -c "${RUN_DIR}/${CONFIG_DIR}"
 
-# run step 2 of pipeline (CREST)
-# python "${PBS_O_WORKDIR}/${SRC_DIR}/step2.py" -o "${RUN_DIR}" -c "${RUN_DIR}/${CONFIG_DIR}"
